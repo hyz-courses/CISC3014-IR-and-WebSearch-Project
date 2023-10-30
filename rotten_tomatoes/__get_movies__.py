9bff2a753e645ccf61135114f5f173d8b938a155
YanzhenHuang <huangyanzhen0108@163.com>
2023-10-31 03:47:33 +0800

    :art: Add some comments

--- rotten_tomatoes/__get_movies__.py
+++ rotten_tomatoes/__get_movies__.py
@@ -1,3 +1,4 @@
+# Written by Huang Yanzhen at Oct 30, 2023
 
 import scrapy
 import re
@@ -25,28 +26,34 @@ class RTMovieCrawler(scrapy.Spider):
         # Local settings
         'LANG': 'EN',
         'USE_EXACT_VALUES': True,
-        'SAVE_DATA': False,
+        'SAVE_DATA': False,     # Don't alter this!!! otherwise you'd create duplicate rows in the excel file
         'ROUND_LEVEL': 3,
     }
 
+    # These aren't really useful yet...
     movie_data = []
     article_start_urls = []
     index = 1
+
     def parse(self, response):
+        # Entire movie grid
         movie_list = response.xpath("//div[@class='flex-container']")
         for movie in movie_list:
             # Father element: Overall container
             score_container = movie.xpath(".//a[@data-track='scores']")
             score_link = score_container.xpath("@href").get()
+
             # Title
             movie_title = score_container.xpath(".//span[@class='p--small']/text()").get()
             # Guardian method. Don't get useless message
             if movie_title is None:
                 continue
-            movie_title = re.sub(r"\n|\s","", movie_title)
+            movie_title = re.sub(r"\n|\s","", movie_title)      # In fact the movie title from rt is kinda sussy
+
             # Stream time
             stream_time = score_container.xpath(".//span[@class='smaller']/text()").get()
             stream_time = re.sub(r"\n|\s|Streaming", "", stream_time)
+
             # Overall score container
             evaluation_scores = score_container.xpath(".//score-pairs-deprecated")
             # Four important scores
@@ -56,6 +63,7 @@ class RTMovieCrawler(scrapy.Spider):
             critics_score = evaluation_scores.xpath("@criticsscore").get()
 
             # Processing data
+            # Change negative/positive to binary expressions, which is a lot easier to cope with
             bin_aud_sentiment = 0 if aud_sentiment == 'negative' else 1
             bin_critics_sentiment = 0 if critics_sentiment == 'negative' else 1
             data = {
@@ -68,6 +76,7 @@ class RTMovieCrawler(scrapy.Spider):
                 "critics sentiment": bin_critics_sentiment,
             }
 
+            # Append movie data... not really useful, cuz i plug it into excel at each loop anyways
             self.movie_data.append(data)
 
             # Store the data in a new Excel file
@@ -75,6 +84,7 @@ class RTMovieCrawler(scrapy.Spider):
             if self.custom_settings['SAVE_DATA']:
                 __save_data__.save_data_to_excel(data)
 
+            # Print beautiful data in console :)
             print("--------------------")
             print(
                 str(self.index) + '\n' +
@@ -86,6 +96,8 @@ class RTMovieCrawler(scrapy.Spider):
                 "critics sentiment: " + critics_sentiment + '\n' +
                 "critics score: " + critics_score
             )
+
+            # This thread is async so I have to do this to omit race conditions
             self.index += 1
 
 
